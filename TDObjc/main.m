64343975d5c3c1f19416fa19bee1af0bb7ef12d5
fangbo <66934854@qq.com>
2020-09-22 20:18:18 +0800

    初次提交

---
+++ TDObjc/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  KCObjc
+//
+//  Created by Cooci on 2020/7/24.
+//
+
+#import <Foundation/Foundation.h>
+#import "TDPerson.h"
+#import <objc/runtime.h>
+
+
+// cache_t
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        TDPerson *p  = [TDPerson alloc];
+        Class pClass = [TDPerson class];
+        [p sayHello];
+        [p sayCode];
+        [p sayMaster];
+//        [p sayNB];
+
+
+        NSLog(@"%@",pClass);
+    }
+    return 0;
+}
